3aecb792e2588c8bde141fa8ad239305baaa1f1a
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-10-23 21:24:53 +0300

    Удаление закомментированного кода от предыдущих уроков

--- js/main.js
+++ js/main.js
@@ -7,42 +7,10 @@ let money = 123,
     mission = 45678,
     period = 2;
 
-// alert("Любой текст");
-
-// console.log("Тоже любой, но уже другой текст");
-
-
-
-// - Вывести в консоль тип данных значений переменных money, income, deposit;
-// console.log("Типы данных значений переменных:");
-// console.log('typeof money;: ', typeof money);
-// console.log('typeof income: ', typeof income);
-// console.log('typeof deposit: ', typeof deposit);
-// console.log();
-
-// - Вывести в консоль длину строки income
-// console.log("Длина строки:");
-// console.log('income.length: ', income.length);
-// console.log();
-
-// - Вывести в консоль “Период (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
-// console.log("Вывести в консоль:");
-// console.log(`Период ${period} месяцев`);
-// console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
-// console.log();
-
-// - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
-// console.log("Строка addExpenses в виде массива строк в нижнем регистре:");
-// console.log('addExpenses.toLowerCase().split(", "): ', addExpenses.toLowerCase().split(", "));
-// console.log();
 
 /* - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30),
 вывести в консоль результат и остаток от деления */
 let budgetDay = 100/30;
-// console.log("Значение переменной budgetDay и остаток от деления чисел, из которых она была получена:");
-// console.log('budgetDay: ', budgetDay);
-// console.log('Остаток от деления 100 / 30: ', 100 % 30);
-// console.log();
 
 
 function requestNumber(q) {
@@ -114,10 +82,7 @@ money = requestNumber("Ваш месячный доход?");
 
 /* Спросить у пользователя “Перечислите возможные расходы за рассчитываемый период через запятую”,
 сохранить в переменную addExpenses, вывести в консоль в виде массива*/
-// console.log("Возможные расходы за рассчитываемый период:");
 addExpenses = requestValue("Перечислите возможные расходы за рассчитываемый период через запятую.");
-// console.log('addExpenses: ', addExpenses);
-// console.log();
 
 /* Спросить у пользователя “Есть ли у вас депозит в банке?”
 и сохранить данные в переменной deposit (булевое значение true/false) */
@@ -128,9 +93,6 @@ let showTypeOf = function(data) {
   console.log(data, typeof data);
 };
 console.log("Типы данных значений переменных 'money', 'income' и 'deposit':");
-// console.log('typeof money: ', typeof money);
-// console.log('typeof income: ', typeof income);
-// console.log('typeof deposit: ', typeof deposit);
 showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
@@ -152,47 +114,11 @@ requiredSum2 = requestNumber("Во сколько это обойдется?");
 
 /* Вычислить доход за месяц, учитывая обязательные расходы,
 сохранить в переменную budgetMonth и вывести результат в консоль*/
-// console.log("Месячный бюджет:");
 let budgetMonth = money - requiredSum1 - requiredSum2;
-// console.log('budgetMonth: ', budgetMonth);
-// console.log();
-
-/* Зная budgetMonth, посчитать за сколько месяцев будет достигнута цель mission,
-вывести в консоль, округляя в большую сторону */
-// console.log(`Количество месяцев до достижения цели "mission" равной ${mission} при текущем бюджете:`);
-// console.log('Math.ceil(mission / budgetMonth): ', Math.ceil(mission / budgetMonth));
-// console.log();
 
 /* Поправить budgetDay учитывая бюджет на месяц, а не месячный доход.
 Вывести в консоль округлив в меньшую сторону (методы объекта Math в помощь) */
-// console.log("Дневной бюджет, исходя из месячного:");
 budgetDay = budgetMonth / 30;
-// console.log('Math.floor(budgetDay): ', Math.floor(budgetDay));
-
-// Написать конструкцию условий
-//     Если budgetDay больше 800, то “Высокий уровень дохода”
-//     Если budgetDay больше 300 и меньше 800, то сообщение “Средний уровень дохода”
-//     Если budgetDay больше 0 и меньше 300 то в консоль вывести сообщение “Низкий уровень дохода”
-//     Если отрицательное значение то вывести “Что то пошло не так”
-//     учесть варианты 0, 300 и 800
-// switch (true) {
-//   case (budgetDay > 800):
-//     console.log("Высокий уровень дохода");
-//     break;
-//   case (budgetDay > 300):
-//     console.log("Средний уровень дохода");
-//     break;
-//   case (budgetDay > 0):
-//     console.log("Низкий уровень дохода");
-//     break;
-//   case (budgetDay < 0):
-//     console.log("Что-то пошло не так");
-//     break;
-//   case (budgetDay === 800):
-//   case (budgetDay === 300):
-//   case (budgetDay === 0):
-//     console.log("Вы попали на границы между уровнями дохода. Определитесь уже...");
-// }
 
 let getStatusIncome = function() {
   // возвращает значение уровня дохода
