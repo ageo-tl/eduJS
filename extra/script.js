d538f9aaf326cc3b6022f545d927a8d8cfd9b963
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-10-29 21:12:32 +0300

    Вывод на страницу текущей даты и времени

--- extra/script.js
+++ extra/script.js
@@ -1,43 +1,21 @@
 "use strict";
 
-// Создать массив week и записать в него дни недели в виде строк
-// - Вывести на экран все дни недели
-// - Каждый из них с новой строчки
-// - Выходные дни - курсивом
-// - Текущий день - жирным шрифтом(использовать объект даты)
-
-let week = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
-
-
-let now = new Date();
-let getDayOfWeek = function(date) {
-  if (date.getDay() === 0) {
-    return 6;
-  } else {
-    return date.getDay() - 1;
-  }
+// Вспомогательные функции
+const dateFormat = function(d) {
+  return d.getHours() + ":" +
+          d.getMinutes() + ":" +
+          d.getSeconds() + " " +
+          d.getDate() + "." +
+          d.getMonth() + "." + d.getFullYear();
 };
-let dayOfWeek = getDayOfWeek(now);
 
+// Работа с датой
+const d = new Date();
 
-let div = document.createElement("div");
+const h = document.createElement("h2");
+h.innerText = dateFormat(d);
+h.style.margin = "50px";
 
-let head = document.createElement("h1");
-head.innerText = "Дни недели:";
-div.appendChild(head);
-
-let dayElem;
-for (let i in week) {
-  dayElem = document.createElement("p");
-  dayElem.innerText = week[i];
-  dayElem.style.marginLeft = "20px";
-  if (+i > 4) {
-    dayElem.style.fontStyle = "italic";
-  }
-  if (+i === dayOfWeek) {
-    dayElem.style.fontWeight = "bold";
-  }
-  div.appendChild(dayElem);
-}
-
-document.body.appendChild(div);
+const dt = document.createElement("div");
+dt.appendChild(h);
+document.body.appendChild(dt);
