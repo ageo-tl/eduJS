af9a3a627e40a7fbb56c0c5942bf5dc746ee2bb0
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-10-29 21:13:32 +0300

    Функция форматирования чисел

--- extra/script.js
+++ extra/script.js
@@ -1,12 +1,19 @@
 "use strict";
 
 // Вспомогательные функции
+const numFrmt = function(n) {
+  if (isNaN(n)) {
+    return n;
+  } else {
+    return n.toLocaleString(undefined, {minimumIntegerDigits: 2});
+  }
+};
 const dateFormat = function(d) {
-  return d.getHours() + ":" +
-          d.getMinutes() + ":" +
-          d.getSeconds() + " " +
-          d.getDate() + "." +
-          d.getMonth() + "." + d.getFullYear();
+  return numFrmt(d.getHours()) + ":" +
+          numFrmt(d.getMinutes()) + ":" +
+          numFrmt(d.getSeconds()) + " " +
+          numFrmt(d.getDate()) + "." +
+          numFrmt(d.getMonth()) + "." + d.getFullYear();
 };
 
 // Работа с датой
