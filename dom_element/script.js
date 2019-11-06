43bf1bc11e4a94b80ecdd792ac9723e5ced977cd
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-11-06 20:56:24 +0300

    Урок #10, дополнительное задание

--- dom_element/script.js
+++ dom_element/script.js
@@ -12,17 +12,48 @@ const DomElement = function(selector, height, width, bg, fontSize) {
                         : this.selector[0] === "#"
                           ? "p"
                           : "hr";
-    const el = document.createElement(elemType);
-    el.style.cssText=`height: ${this.height};
+    this.el = document.createElement(elemType);
+    this.el.style.cssText=`height: ${this.height};
                            width: ${this.width};
                            background: ${this.bg};
                            fontSize: ${this.fontSize};
                           `;
-    el.textContent = "внутрь записывать любой текст";
-    document.body.appendChild(el);
+    this.el.textContent = "внутрь записывать любой текст";
+    document.body.appendChild(this.el);
   };
 };
 
+const arr = [37, 38, 39, 40];
+const pushArrows = function(event) {
+  const key = event.keyCode;
+  let top, left;
+  if (arr.includes(key)) {
+    switch (key) {
+      case 37:
+        // влево
+        left = this.style.left ? parseInt(this.style.left) : 0;
+        this.style.left = left - 10 + "px";
+        break;
+      case 38:
+        // вверх
+        top = this.style.top ? parseInt(this.style.top) : 0;
+        this.style.top = top - 10 + "px";
+        break;
+      case 39:
+        // вправо
+        left = this.style.left ? parseInt(this.style.left) : 0;
+        this.style.left = left + 10 + "px";
+        break;
+      case 40:
+        // вниз
+        top = this.style.top ? parseInt(this.style.top) : 0;
+        this.style.top = top + 10 + "px";
+    }
+  }
+};
 
 const myElem = new DomElement( ".block", "100px", "100px", "silver", "1rem");
 myElem.create();
+
+myElem.el.style.position = "absolute";
+document.addEventListener("keydown", pushArrows.bind(myElem.el));
